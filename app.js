05f98576416577630421b17287ae48a606a783d4
Ste11front <137266358+Ste11front@users.noreply.github.com>
2023-07-30 23:06:18 +0200

    es app.js

--- app.js
+++ app.js
@@ -58,3 +58,77 @@ function transFormData(data){
 
 
 }
+
+// Esercizio
+
+// const fs = require("fs");
+
+// const inputUrl = process.argv[2];
+// const outputUrl = process.argv[3];
+
+// let data = readFile(inputUrl)
+
+// if (data) {
+
+//     const result = transFormData(data)
+
+//     writeData(outputUrl, result);
+// }
+
+
+// function readFile(url){
+//     try {
+//         const data = fs.readFileSync(url, "utf8");
+//         return data;
+//     } catch (err) {
+//         console.error(err.message);
+//         return null;
+//     }
+// }
+
+
+// function writeData(url, data){
+//     try {
+//         fs.writeFileSync(url, data);
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// }
+
+// function transFormData(data){
+
+//     const rows = data.split(/\r?\n/);
+
+//     //1) creare una costante 'header' con la prima riga che AVRETE TOLTO a rows;
+//     const header = rows.shift();
+    
+//     //2) create una constante 'headerArray' splittando la stringa header sulle virgole;
+//     const headerArray = header.split(",");
+
+//     //3) crate un array chiamato students (vuoto);
+//     const students = [];
+
+//     //4) ciclate sull'array rows;
+//     for (let i = 0; i < rows.length; i++) {
+//         //4a) create una costante rowArray splittando la singola row sulle virgole;
+//         const rowArray = rows[i].split(",");
+
+//         //4b) create un oggetto vuoto chiamato student;
+//         const student = {};
+
+//         //4c) ciclate sull'headerArray;
+//         for (let j = 0; j < headerArray.length; j++) {
+//             //4c1) per ogni elemento dell'headerArray aggiungo una proprietà all'oggetto student
+//             // student[headerArray[j]] = rowArray[j];
+//             student[headerArray[j]] = rowArray[j];
+//         }
+
+//         //4d) aggiungo student a students
+//         students.push(student);
+//     }
+
+//     //5) ritorno JSON.stringify di students
+//     return JSON.stringify(students);
+
+
+// }
