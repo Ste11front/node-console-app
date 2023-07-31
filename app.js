cdc70e733e1906c4bd392b170f7066a2c932dbdb
Ste11front <137266358+Ste11front@users.noreply.github.com>
2023-07-31 13:18:21 +0200

    node

--- app.js
+++ app.js
@@ -1,22 +1,39 @@
 const fs = require("fs");
+// const transformCsvToJson = require('./json-converter');
+// const transformJsonToCsv = require('./csv-converter');
+// const jsonConverter = require('./json-converter');
+// console.log(jsonConverter);
 
 const inputUrl = process.argv[2];
+const splittedInputUrl = inputUrl.split('.');
+const ext = splittedInputUrl[splittedInputUrl.length - 1];
+
+let transformFunction;
+if (ext.toLowerCase().includes('json')) {
+    transformFunction = require('./csv-converter');
+} else if (ext.toLowerCase().includes('csv')){
+    transformFunction = require('./json-converter');
+} else {
+    console.log('non posso convertire i file: ' + ext);
+    process.exit();
+}
+
 const outputUrl = process.argv[3];
+
 let divider = process.argv[4];
-if (divider === undefined) { //A2)aggiungere un parametro alla applicazione che mi permette di indicare il carattere divisorio
+if (divider === undefined) { //A2)aggiungere un parametro all'applicazione che permetta di indicare il carattere divisorio
     divider = ',';
 }
 console.log(divider);
+
 
 let data = readFile(inputUrl)
 
 if (data) {
 
-    const result = transFormData(data)
-
+    const result = transformFunction(data, divider)
     writeData(outputUrl, result);
 }
-
 
 function readFile(url){
     try {
@@ -28,87 +45,11 @@ function readFile(url){
     }
 }
 
-
 function writeData(url, data){
     try {
         fs.writeFileSync(url, data);
     } catch (err) {
         console.error(err.message);
-    }
-}
-
-function transFormData(data){
-
-    const rows = data.split(/\r?\n/);
-
-    //1) creare una costante 'header' con la prima riga che AVRETE TOLTO a rows;
-    const header = rows.shift();
-    console.log(header);
-
-    //2) creare una constante 'headerArray' splittando la stringa header sulle virgole;
-    const headerArray = header.split(divider);
-    console.log(headerArray);
-
-    //3) crare un array chiamato elements (vuoto);
-    const elements = [];
-    console.log(elements);
-
-    //4) ciclare sull'array rows;
-    for (let i = 0; i < rows.length; i++) {
-        const row = rows[i];
-        console.log(row);
-
-        //4a) creare una costante rowArray splittando la singola row sulle virgole;
-        const rowArray = row.split(divider);
-        console.log(rowArray);
-
-        //4b) creare un oggetto vuoto chiamato element;
-        const element = {};
-        console.log(element);
-
-        //4c) ciclare sull'headerArray;
-        for (let j = 0; j < headerArray.length; j++) {
-            const key = headerArray[j];
-            console.log(key);
-            const value = rowArray[j];
-            const trimmedValue = value.trim(); //A3)gestire la possibilità che nel csv ci siano degli spazi non voluti 
-            const convertedValue = tryParse(trimmedValue);
-
-            //4c1) per ogni elemento dell'headerArray aggiungere una proprietà all'oggetto element
-            // element[headerArray[j]] = rowArray[j];
-            element[key] = convertedValue; 
-            console.log(element); 
-        }
-        
-        //4d) aggiungere element a elements
-        elements.push(element);
-        console.log(elements);
-    }
-
-    //5) ritornare JSON.stringify di elements
-    return JSON.stringify(elements, null, 4);
-
-    // return JSON.stringify(rows, null, 4);
- 
-}
-
-function tryParse(str) { //A1)tipizzare i valori nel json
-    
-    // if (str === 'true' || str === 'false') {
-    //     return str === 'true';
-    // }
-    if (str === 'true') {
-        return true;
-    }
-    if (str === 'false') {
-        return false;
-    }
-    const number = parseFloat(str);
-    console.log(number);
-    if (isNaN(number)) {
-        return str;
-    } else {
-        return number;
     }
 }
 
--- json-converter.js
+++ json-converter.js
@@ -3,50 +3,54 @@ function transformFromCsvToJson(data, divider){
     const rows = data.split(/\r?\n/);
 
     //1) creare una costante 'header' con la prima riga che AVRETE TOLTO a rows;
-
     const header = rows.shift();
     console.log(header);
-    //2) create una constante 'headerArray' splittando la stringa header sulle virgole;
+
+    //2) creare una constante 'headerArray' splittando la stringa header sulle virgole;
     const headerArray = header.split(divider);
     console.log(headerArray);
-    //3) crate un array chiamato students (vuoto);
+
+    //3) crare un array chiamato elements (vuoto);
     const elements = [];
     console.log(elements);
-    //4) ciclate sull'array rows;
+
+    //4) ciclare sull'array rows;
     for (let i = 0; i < rows.length; i++) {
         const row = rows[i];
         console.log(row);
-        //4a) create una costante rowArray splittando la singola row sulle virgole;
+
+        //4a) creare una costante rowArray splittando la singola row sulle virgole;
         const rowArray = row.split(divider);
         console.log(rowArray);
-        //4b) create un oggetto vuoto chiamato student;
+
+        //4b) creare un oggetto vuoto chiamato element;
         const element = {};
         console.log(element);
-         //4c) ciclate sull'headerArray;
+
+        //4c) ciclare sull'headerArray;
         for (let j = 0; j < headerArray.length; j++) {
             const key = headerArray[j];
             console.log(key);
             const value = rowArray[j];
             const trimmedValue = value.trim(); //A3)gestire la possibilità che nel csv ci siano degli spazi non voluti 
             const convertedValue = tryParse(trimmedValue);
-            //4c1) per ogni elemento dell'headerArray aggiungo una proprietà all'oggetto student
-            // student[headerArray[j]] = rowArray[j];
+
+            //4c1) per ogni elemento dell'headerArray aggiungere una proprietà all'oggetto element
+            // element[headerArray[j]] = rowArray[j];
             element[key] = convertedValue; 
-            console.log(element);
-            
+            console.log(element); 
         }
-          //4d) aggiungo student a students
+        
+        //4d) aggiungere element a elements
         elements.push(element);
         console.log(elements);
     }
-    //5) ritorno JSON.stringify di students
+
+    //5) ritornare JSON.stringify di elements
     return JSON.stringify(elements, null, 4);
-    
-        
-    
+
     // return JSON.stringify(rows, null, 4);
-
-
+ 
 }
 
 function tryParse(str) { //A1)tipizzare i valori nel json
@@ -67,7 +71,6 @@ function tryParse(str) { //A1)tipizzare i valori nel json
     } else {
         return number;
     }
-  
 }
 
 // module.exports = {transform: transformFromCsvToJson, parse: tryParse}
